3024fc114569f529452a1339497c6c6fa0c7305e
Raghuraj Singh <hannuseven@gmail.com>
2024-09-10 12:35:51 +0530

    scoreboard created

---
+++ scoreboard.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+ALIGN = "center"
+FONT = ('Arial', 13, 'normal')
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.write(f"Score = {self.score}", align=ALIGN, font= FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score = {self.score}", align=ALIGN, font= FONT)
