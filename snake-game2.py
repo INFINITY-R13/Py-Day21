2c246b1839f0633c9d1c7c2b9ae210b40a3ced3f
Raghuraj Singh <hannuseven@gmail.com>
2024-09-10 12:05:24 +0530

    detect collisions with food

--- snake-game2.py
+++ snake-game2.py
@@ -1,6 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import Snake 
+from food import Food
 
 
 screen = Screen()
@@ -10,6 +11,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,8 +23,10 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 screen.exitonclick()    
